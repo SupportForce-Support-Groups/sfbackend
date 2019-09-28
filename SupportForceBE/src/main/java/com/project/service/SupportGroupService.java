d398eee331e9df57345e9c9a5783e21fa37ff0f1
Easley Boo <easley.boo.sn@gmail.com>
2019-09-27 20:00:12 -0600

    Addition of all Support Groups controller method.

--- SupportForceBE/src/main/java/com/project/service/SupportGroupService.java
+++ SupportForceBE/src/main/java/com/project/service/SupportGroupService.java
@@ -1,9 +1,13 @@
 package com.project.service;
 
+import java.util.List;
+
 import com.project.model.SupportGroup;
 
 public interface SupportGroupService {
 	
 	public SupportGroup creationOfSupportGroup(String supportGroupName, int addictionId);
+	
+	public List<SupportGroup> selectAllSupportGroups();
 
 }
